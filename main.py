4a950858a815d5497371400cceb5d1c1a1c0c8a0
Maxwellkwhite <maxwellkw@comcast.net>
2024-07-31 16:40:52 -0400

    Likely last commit for now

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 from tkinter import *
 from tkinter import filedialog, messagebox
 from PIL import Image, ImageFont, ImageDraw, ImageTk
+from customtkinter import CTkEntry, CTk, CTkLabel, CTkButton
+import customtkinter
 
 original_file_path = '/Users/maxwellwhite/Documents/PythonPractice/Final_Projects/watermark/1.png'
 
-def image_upload():
-    file_path = filedialog.askopenfilename()
-    if file_path:
-        image = Image.open(file_path)
-        image.save(file_path)
-        messagebox.showinfo("Success", "Image uploaded successfully!")
-    else:
-        messagebox.showinfo("No file was selected. Please choose a file.")
-
 
 def create_image():
-    file_path = filedialog.askopenfilename()
-    if file_path:
+    file_path = filedialog.askopenfilename(filetypes=[("Allowed Types", '*.jpeg *.jpg *.png')])
+    if len(file_path):
         image = Image.open(file_path)
         image.save(original_file_path)
         messagebox.showinfo("Success", "Image uploaded successfully!")
@@ -36,19 +29,25 @@ def create_image():
 
         out.show()
 
+customtkinter.set_appearance_mode("light")
+customtkinter.set_default_color_theme("dark-blue")
 
-window = Tk()
+window = CTk()
 window.title("Watermark App")
 window.config(padx=100, pady=50)
 
-title = Label(text="Watermark App")
-title.grid(column=0, row=0)
-title.config(pady=20)
+title = CTkLabel(text="The Mark of Water", master=window, font=("Arial",30, "bold"))
+title.grid(column=0, row=0, pady=10, columnspan=2)
 
-entry_text = Entry()
-entry_text.grid(column=0, row=2)
+instructions = CTkLabel(text="Type your watermark below, then click the button.", master=window, font=("Arial",15))
+instructions.grid(column=0, row=1, pady=10, columnspan=2)
+# entry_label = CTkLabel(text="Enter Watermark Text Here:", master=window)
+# entry_label.grid(column=1, row=1)
 
-start_button = Button(text="Start", command=create_image)
-start_button.grid(column=0, row=3)
+entry_text = CTkEntry(master=window, placeholder_text="Watermark Text",)
+entry_text.grid(column=0, row=2, padx=10)
+
+start_button = CTkButton(text="Find Image", command=create_image, master=window)
+start_button.grid(column=1, row=2)
 
 window.mainloop()
